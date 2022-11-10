fb38829a492b1d36779ea7a84cdb6c9760f780f3
peter988 <116488083+peter988@users.noreply.github.com>
2022-11-10 21:32:23 +0200

    Update settings.py

--- amazon2/settings.py
+++ amazon2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-kws0@vp&z=j406%ef%)ev@5^=2ltc!j%=k0^p&nyk7ev$k3_i%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','https://myamazonon.herokuapp.com/']
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/','https://myamazonon.herokuapp.com/']
 
 
 # Application definition
