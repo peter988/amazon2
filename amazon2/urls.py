d3458168591bc44e42259d3be820d5fa26ac7feb
peter988 <116488083+peter988@users.noreply.github.com>
2022-11-10 22:33:34 +0200

    Update urls.py

--- amazon2/urls.py
+++ amazon2/urls.py
@@ -24,7 +24,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("add/", add),
     path("amazon/", seller),
-
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
